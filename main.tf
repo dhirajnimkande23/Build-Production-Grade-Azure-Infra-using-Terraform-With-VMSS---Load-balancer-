2055e6184d8a863e501f012cdc86b7313e22d62b
Dhiraj Nimkande <dhirajnimkande23@gmail.com>
2026-04-14 22:56:04 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,179 @@
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+}
+
+# Create a resource group
+resource "azurerm_resource_group" "main" {
+  name     = var.resource_group_name
+  location = var.location
+
+  tags = var.tags
+}
+
+# Create a virtual network
+resource "azurerm_virtual_network" "main" {
+  name                = var.vnet_name
+  address_space       = [var.vnet_address_space]
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  tags = var.tags
+}
+
+# Create a subnet
+resource "azurerm_subnet" "internal" {
+  name                 = var.subnet_name
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = [var.subnet_address_prefix]
+}
+
+# Create public IP for Load Balancer
+resource "azurerm_public_ip" "main" {
+  name                = var.public_ip_name
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  allocation_method   = "Static"
+  sku                = "Standard"
+
+  tags = var.tags
+}
+
+# Create Load Balancer
+resource "azurerm_lb" "main" {
+  name                = var.load_balancer_name
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  sku                = "Standard"
+
+  frontend_ip_configuration {
+    name                 = var.frontend_ip_name
+    public_ip_address_id = azurerm_public_ip.main.id
+  }
+
+  tags = var.tags
+}
+
+# Create Load Balancer Backend Pool
+resource "azurerm_lb_backend_address_pool" "main" {
+  loadbalancer_id = azurerm_lb.main.id
+  name            = var.backend_pool_name
+}
+
+# Create Load Balancer Health Probe
+resource "azurerm_lb_probe" "main" {
+  loadbalancer_id = azurerm_lb.main.id
+  name            = var.health_probe_name
+  port            = 80
+  protocol        = "Tcp"
+  interval_in_seconds = 15
+  number_of_probes    = 4
+}
+
+# Create Load Balancer Rule
+resource "azurerm_lb_rule" "main" {
+  loadbalancer_id                = azurerm_lb.main.id
+  name                           = var.lb_rule_name
+  protocol                       = "Tcp"
+  frontend_port                  = 80
+  backend_port                   = 80
+  frontend_ip_configuration_name = var.frontend_ip_name
+  backend_address_pool_ids       = [azurerm_lb_backend_address_pool.main.id]
+  probe_id                       = azurerm_lb_probe.main.id
+  idle_timeout_in_minutes        = 4
+  tcp_reset_enabled              = true
+}
+
+# Create Network Security Group and rule
+resource "azurerm_network_security_group" "main" {
+  name                = var.nsg_name
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  security_rule {
+    name                       = "HTTP"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 1002
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  tags = var.tags
+}
+
+# Associate Network Security Group to the subnet
+resource "azurerm_subnet_network_security_group_association" "main" {
+  subnet_id                 = azurerm_subnet.internal.id
+  network_security_group_id = azurerm_network_security_group.main.id
+}
+
+# Create Virtual Machine Scale Set
+resource "azurerm_linux_virtual_machine_scale_set" "main" {
+  name                = var.vmss_name
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  sku                 = var.vm_sku
+  instances           = var.instance_count
+  admin_username      = var.admin_username
+  upgrade_mode        = "Automatic"
+
+  disable_password_authentication = true
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-focal"
+    sku       = "20_04-lts-gen2"
+    version   = "latest"
+  }
+
+  os_disk {
+    storage_account_type = "Premium_LRS"
+    caching              = "ReadWrite"
+  }
+
+  admin_ssh_key {
+    username   = var.admin_username
+    public_key = var.ssh_public_key != "" ? var.ssh_public_key : file("~/.ssh/id_rsa.pub")
+  }
+
+  network_interface {
+    name    = "internal"
+    primary = true
+
+    ip_configuration {
+      name                                   = "internal"
+      primary                                = true
+      subnet_id                             = azurerm_subnet.internal.id
+      load_balancer_backend_address_pool_ids = [azurerm_lb_backend_address_pool.main.id]
+    }
+  }
+
+  extension {
+    name                 = "install-webserver"
+    publisher            = "Microsoft.Azure.Extensions"
+    type                 = "CustomScript"
+    type_handler_version = "2.1"
+    settings = jsonencode({
+      "script" = base64encode(templatefile("${path.module}/scripts/install-webserver.sh", {}))
+    })
+  }
+
+  tags = var.tags
+}
